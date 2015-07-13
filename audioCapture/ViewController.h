7e0eb67f4e7e3f419914ff27ceb383097bb9d35e
Navaneethan Sivabalaviknarajah <niruns@gmail.com>
2015-07-13 13:02:25 -0400

    Initial Commit

---
+++ audioCapture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  audioCapture
+//
+//  Created by Navaneethan Sivabalaviknarajah on 2015-07-13.
+//  Copyright (c) 2015 Navaneethan Sivabalaviknarajah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ audioCapture/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  audioCapture
+//
+//  Created by Navaneethan Sivabalaviknarajah on 2015-07-13.
+//  Copyright (c) 2015 Navaneethan Sivabalaviknarajah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
